fb71d3f4e94e41789c26e3784617d3e09cfeb17a
Dawei Huang <dwhuang9@gmail.com>
2012-08-06 14:38:33 +0800

    asdf

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 git add .
-git commit -m "asdf"
+git commit -a -m "asdf"
 git push origin master
--- uva/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-
-int main(){
-  printf("%.1f\n", 0.04);
-  printf("%.1f\n", 0.05);
-  return  0;
-}
